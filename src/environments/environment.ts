7c8ac196b4657aa75275fd87b5f316814307c40f
Franco Cortez <fcortez@chattigo.com>
2019-09-24 15:29:27 -0300

    MODIFY | Se generan modificaciones en el comportamiento, se mejora control de envio de archivos con bloqueo de input, se realiza nueva configuracion del socket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 const globalBffServer = 'https://k8s-dev.chattigo.com';
+
 export const environment = {
   production: false,
   socketServer: 'https://k8s-dev.chattigo.com',

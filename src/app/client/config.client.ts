aa244a869e627042010b1f2409ad519d4d7721ba
Franco Cortez <fcortez@chattigo.com>
2019-10-01 18:25:29 -0300

    ADD | Se agregan mejoras para pantalla pre login para seleccion de formulario. FIX | Se modifica comportamiento el firefox.

---
+++ src/app/client/config.client.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError, map} from 'rxjs/operators';
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigClient {
+
+  constructor(private readonly http: HttpClient) {}
+
+  public getConfig(did): Observable<any> {
+    return this.http.get<HttpResponse<any>>(`${environment.configPath}/${did.replace('@', '-')}-${environment.configProfile}.json`).pipe(
+      map(data => data),
+      catchError(err => {
+        return throwError(err);
+      })
+    );
+  }
+}

a6c70477da9b530616d125f3ffdd8f6732c580eb
Franco Cortez <fcortez@chattigo.com>
2019-10-18 13:14:25 -0300

    FIX | Correccion del socket cuando se pierde conexion

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -19,16 +19,19 @@ export class MessageService {
   }
 
   public sendMessage(messageUI: MessageUiModel, messageDto: MessageDto) {
-    messageDto.content = messageUI.originalContent;
-    messageDto.type = messageUI.type;
-    messageDto.timestamp = messageUI.hour;
-    messageDto.id = uuid();
-    messageDto.isAttachment = !(messageUI.mediaUrl == null);
-    messageDto.attachment = {
+    const mess: MessageDto = {
+      ... messageDto
+    };
+    mess.content = messageUI.originalContent;
+    mess.type = messageUI.type;
+    mess.timestamp = messageUI.hour;
+    mess.id = uuid();
+    mess.isAttachment = !(messageUI.mediaUrl == null);
+    mess.attachment = {
       mediaUrl: messageUI.redirectUrl,
       mimeType: messageUI.mimeType
     };
-    this.socketClient.sendMessage(messageDto);
+    this.socketClient.sendMessage(mess);
     return of(messageUI);
   }
 
